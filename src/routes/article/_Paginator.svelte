be45b3eb33ee1278b24b8c967f9e943ee5377ee8
Jitesh Doshi <jitesh@spinspire.com>
2020-02-19 15:40:19 -0500

    adds rel=prefetch to paginator

--- src/routes/article/_Paginator.svelte
+++ src/routes/article/_Paginator.svelte
@@ -1,13 +1,15 @@
 <script>
   export let pg = 0;
   export let max;
+  export let prefetch = true;
   import { stores } from "@sapper/app";
   const { page } = stores();
+  const rel = prefetch ? "prefetch" : "";
 </script>
 
 <div class="text-center mt-2">
   {#if pg > 0}
-    <a href="{$page.path}?pg={pg - 1}" class="btn btn-primary btn-sm">
+    <a href="{$page.path}?pg={pg - 1}" class="btn btn-primary btn-sm" {rel}>
       <i class="material-icons">keyboard_arrow_left</i>
       Prev
     </a>
@@ -18,7 +20,7 @@
     </span>
   {/if}
   {#if pg < Math.floor(max)}
-    <a href="{$page.path}?pg={pg + 1}" class="btn btn-primary btn-sm">
+    <a href="{$page.path}?pg={pg + 1}" class="btn btn-primary btn-sm" {rel}>
       Next
       <i class="material-icons">keyboard_arrow_right</i>
     </a>
